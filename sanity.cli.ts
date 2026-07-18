141786643d62f9f0c2262f03fea8a4901138da43
yuri <yuridelimaandrade34@gmail.com>
2026-07-18 11:08:36 -0300

    chore: add sanity deployment appId

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,4 +5,7 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID || process.env.VITE_SANITY_PROJECT_ID || '',
     dataset: process.env.SANITY_STUDIO_DATASET || process.env.VITE_SANITY_DATASET || 'production',
   },
+  deployment: {
+    appId: 'rdxwxmdt7cox3ahvwmj0fdq2',
+  },
 });
